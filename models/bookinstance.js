452fb951e0b6d80dd9ad74e5fabf1b1553350c22
Taco <taco-tues@tuta.io>
2019-11-03 03:14:18 -0700

    cleaned up requires

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,7 +1,6 @@
 const mongoose =  require('mongoose');
 const moment   =  require('moment');
-
-const Schema = mongoose.Schema;
+const Schema   =  mongoose.Schema;
 
 const BookInstanceSchema = new Schema(
   {
